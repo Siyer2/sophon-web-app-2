3d554afefc44f69e8c6ac36f55878f36cc30897f
Syam Iyer <iyersyam@gmail.com>
2020-04-09 18:54:08 +1000

    Created left hand side of the front page

--- src/components/FrontPageBody.js
+++ src/components/FrontPageBody.js
@@ -5,9 +5,13 @@ function FrontPageBody() {
         <div className="container">
             <div className="row">
                 <div className="col">
-                    1 of 2
+                    <h3> Download Student Application </h3>
+                    <div className="d-flex justify-content-between">
+                        <button type="button" className="btn btn-secondary">Download Mac Application</button>
+                        <button type="button" className="btn btn-secondary">Download Windows Application</button>
+                    </div>
                 </div>
-                <div className="col">
+                <div class="col">
                     2 of 2
                 </div>
             </div>
