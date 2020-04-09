57fd9a8deea0e3ab9913d11d503d814080cad134
Syam Iyer <iyersyam@gmail.com>
2020-04-09 16:53:08 +1000

    Added the FrontPageBody component

---
+++ src/components/FrontPageBody.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function FrontPageBody() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    1 of 2
+                </div>
+                <div className="col">
+                    2 of 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FrontPageBody;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Navbar from './NavBar';
+import FrontPageBody from './FrontPageBody';
 
 function Home() {
     return (
         <div>
             <Navbar />
+            <FrontPageBody />
         </div>
     )
 }
