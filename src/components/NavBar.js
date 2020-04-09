a8e90089179e190a11f769cc97ac0732094023b4
Syam Iyer <iyersyam@gmail.com>
2020-04-09 16:38:07 +1000

    Added a navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a href="localhost:3000" className="navbar-brand">
+                sophon
+            </a>
+        </nav>
+    )
+}
+
+export default NavBar;
