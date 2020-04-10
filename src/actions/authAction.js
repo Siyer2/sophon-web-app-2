cba93e6a41199726b62c2f46147a54c13d9862d0
Syam Iyer <iyersyam@gmail.com>
2020-04-10 11:59:06 +1000

    Successfully implemented login connection to the backed

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -17,24 +17,27 @@ function finishLogin(data) {
 //==== User Requests ====//
 export function login(email, password) {
     return function(dispatch) {
-        dispatch(requestLogin);
+        dispatch(requestLogin());
 
         return axios({
             method: 'post', 
-            url: 'localhost:5902/login', 
+            url: 'http://localhost:5902/login', 
             data: {
                 email,
                 password
             }
         })
         .then(
-            response => response.json(), 
-            error => console.log("error logging in", error)
+            response => response.data, 
+            error => {
+                console.log("error logging in", error);
+                // TODO: create a failed login action creator
+            }
         )
         .then(
             json => {
                 dispatch(finishLogin(json));
-                // set token to json.token
+                // TODO: set token to json.token
             }
         )
     }
