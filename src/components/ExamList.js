551e3556fc159e5ad7e89fcdafc9d510662ec3d8
Syam Iyer <iyersyam@gmail.com>
2020-04-10 18:13:46 +1000

    Added a logout button

--- src/components/ExamList.js
+++ src/components/ExamList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logout } from '../actions/authAction';
 
 function ExamList() {
     return (
-        <Link to="/">
+        <Link to="/" onClick={() => logout()}>
             <button type="button" className="btn btn-primary">
                 Logout
             </button>
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -5,7 +5,7 @@ export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
         localStorage.getItem('user')
             ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+            : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
     )} />
 )
 
