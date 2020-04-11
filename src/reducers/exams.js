0ffd7dd20d0f5415119a365a28a2faa72f2b0ece
Syam Iyer <iyersyam@gmail.com>
2020-04-11 16:03:14 +1000

    Added exams reducer

---
+++ src/reducers/exams.js
@@ -0,0 +1,32 @@
+const initialState = {
+    isFetching: false,
+    exams: [],
+    error: ''
+}
+
+const auth = (state = initialState, action) => {
+    switch (action.type) {
+        case 'FETCHING_EXAMS':
+            return Object.assign(
+                {},
+                state,
+                { isFetching: true, error: '' }
+            )
+        case 'RECEIVE_EXAMS':
+            return Object.assign(
+                {},
+                state,
+                { isFetching: false, exams: action.exams }
+            )
+        case 'FAILED_RECEIVING_EXAMS':
+            return Object.assign(
+                {},
+                state,
+                { isFetching: false, error: action.error }
+            )
+        default:
+            return state;
+    }
+}
+
+export default auth;
