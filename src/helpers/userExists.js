3d389b713daec1b8e83a1c929fa4c911ee1bad43
Syam Iyer <iyersyam@gmail.com>
2020-04-14 21:34:12 +1000

    Removed auth from userExists

--- src/helpers/userExists.js
+++ src/helpers/userExists.js
@@ -1,35 +1,13 @@
 import { history } from '../helpers/history';
-import config from '../config';
-const api = config.API;
 
 export function userExists() {
     let user = JSON.parse(localStorage.getItem('user'));
     
     if (user && user.token) {
-        return api({
-            method: 'get',
-            url: '/auth',
-            headers: { 'Authorization': 'Bearer ' + user.token }
-        })
-        .then(
-            response => {
-                if (response.status === 200) {
-                    return true;
-                }
-                else {
-                    redirectToHome();
-                    return false;
-                }
-            }
-        )
-        .catch(
-            () => {
-                redirectToHome();
-                return false;
-            }
-        )
+        return true;
     }
     else {
+        redirectToHome();
         return false;
     }
 }
