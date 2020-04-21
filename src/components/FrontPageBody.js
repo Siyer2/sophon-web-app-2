788257cf69ec02b424b0d6331e730bec0af05c4c
Syam Iyer <iyersyam@gmail.com>
2020-04-22 00:33:41 +1000

    Store the code and student id on enter click

--- src/components/FrontPageBody.js
+++ src/components/FrontPageBody.js
@@ -4,7 +4,16 @@ import {
     Button
 } from 'react-bootstrap';
 
-function FrontPageBody() {
+var state = {};
+
+function FrontPageBody(props) {
+    function enterExamClicked(event) {
+        event.preventDefault();
+        console.log("state", state);
+    }
+    function handleChangeText(event, name) {
+        state[name] = event.target.value;
+    }
     return (
         <div className="container px-lg-5">
             <div className="row mx-lg-n5">
@@ -15,15 +24,15 @@ function FrontPageBody() {
                 </div>
                 <div className="col py-3 px-lg-5 border bg-light">
                     <h3> Enter Exam </h3>
-                    <Form>
+                    <Form onSubmit={(e) => { enterExamClicked(e) }}>
                         <Form.Group>
                             <Form.Label>Exam Code</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter Exam Code" />
+                            <Form.Control required type="text" placeholder="Enter Exam Code" onChange={(e) => {handleChangeText(e, "examCode")}}/>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label>Student ID</Form.Label>
-                            <Form.Control required type="text" placeholder="Enter Student ID" />
+                            <Form.Control required type="text" placeholder="Enter Student ID" onChange={(e) => { handleChangeText(e, "studentId") }}/>
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Enter
