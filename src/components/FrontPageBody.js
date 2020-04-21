26f9c92526bbf9654d62fa01e30196180ce7635b
Syam Iyer <iyersyam@gmail.com>
2020-04-22 00:26:16 +1000

    Changed the frontend to enter exam

--- src/components/FrontPageBody.js
+++ src/components/FrontPageBody.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+    Form, 
+    Button
+} from 'react-bootstrap';
 
 function FrontPageBody() {
     return (
@@ -10,13 +14,24 @@ function FrontPageBody() {
                     <button type="button" className="btn btn-secondary">Download Windows Application</button>
                 </div>
                 <div className="col py-3 px-lg-5 border bg-light">
-                    <h3> Sign up for Pilot Program </h3>
-                    <div className="input-group mb-3">
-                        <input type="email" className="form-control" placeholder="Email" />
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Submit</button>
-                        </div>
-                    </div>
+                    <h3> Enter Exam </h3>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Exam Code</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Exam Code" />
+                            <Form.Text className="text-muted">
+                                You would've received this from your Lecturer.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Student ID</Form.Label>
+                            <Form.Control type="text" placeholder="Student ID" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Enter
+                        </Button>
+                    </Form>
 
                 </div>
             </div>
